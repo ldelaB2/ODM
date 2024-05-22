c91eac453f69bb49916a8e3abad1373769d0e951
Will Delabretonne <ldelaB2@outlook.com>
2024-05-22 10:04:33 -0500

    updating config file

--- myconfig.sh
+++ myconfig.sh
@@ -43,6 +43,89 @@ ensure_prereqs() {
     sudo pip3 install -U shyaml
 }
 
-ensure_prereqs
 
-echo "Finished Checking Prereqs"
+# Save all dependencies in snapcraft.yaml to maintain a single source of truth.
+# Maintaining multiple lists will otherwise be painful.
+installdepsfromsnapcraft() {
+    section="$2"
+    case "$1" in
+        build) key=build-packages; ;;
+        runtime) key=stage-packages; ;;
+        *) key=build-packages; ;; # shouldn't be needed, but it's here just in case
+    esac
+
+    UBUNTU_VERSION=$(lsb_release -r)
+    SNAPCRAFT_FILE="snapcraft.yaml"
+    if [[ "$UBUNTU_VERSION" == *"21.04"* ]]; then
+        SNAPCRAFT_FILE="snapcraft21.yaml"
+    fi
+
+    cat snap/$SNAPCRAFT_FILE | \
+        shyaml get-values-0 parts.$section.$key | \
+        xargs -0 sudo $APT_GET install -y -qq --no-install-recommends
+}
+
+
+installreqs() {
+    cd /code
+    
+    ## Set up library paths
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUNPATH/SuperBuild/install/lib
+
+	## Before installing
+    echo "Updating the system"
+    ensure_prereqs
+    
+    echo "Installing Required Requisites"
+    installdepsfromsnapcraft build prereqs
+    echo "Installing OpenCV Dependencies"
+    installdepsfromsnapcraft build opencv
+    echo "Installing OpenSfM Dependencies"
+    installdepsfromsnapcraft build opensfm
+    echo "Installing OpenMVS Dependencies"
+    installdepsfromsnapcraft build openmvs
+    
+    set -e
+
+    # edt requires numpy to build
+    pip install --ignore-installed numpy==1.23.1
+    pip install --ignore-installed -r requirements.txt
+    #if [ ! -z "$GPU_INSTALL" ]; then
+    #fi
+    set +e
+}
+
+
+
+install_odm() {
+    installreqs
+
+    if [ ! -z "$PORTABLE_INSTALL" ]; then
+        echo "Replacing g++ and gcc with our scripts for portability..."
+        if [ ! -e /usr/bin/gcc_real ]; then
+            sudo mv -v /usr/bin/gcc /usr/bin/gcc_real
+            sudo cp -v ./docker/gcc /usr/bin/gcc
+        fi
+        if [ ! -e /usr/bin/g++_real ]; then
+            sudo mv -v /usr/bin/g++ /usr/bin/g++_real
+            sudo cp -v ./docker/g++ /usr/bin/g++
+        fi
+    fi
+
+    set -eo pipefail
+    
+    echo "Compiling SuperBuild"
+    cd ${RUNPATH}/SuperBuild
+    mkdir -p build && cd build
+    cmake .. && make -j$processes
+
+    echo "Configuration Finished"
+}
+
+
+
+# Main Execution
+RUNPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+install_odm
+
+echo "Finished Install dependencies Prereqs"
